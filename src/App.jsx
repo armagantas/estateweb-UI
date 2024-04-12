2ad2b4e0eeec681f508e5714a5d09bc6250b349c
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-12 19:58:54 +0300

    Hero section done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Navbar from "./components/Navbar/Navbar";
 import "./layout.scss";
+import Home from "./routes/Home/Home";
 
 function App() {
   return (
     <div className="layout">
-      <Navbar />
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
---
+++ src/routes/Home/Home.jsx
@@ -0,0 +1,40 @@
+import SearchBar from "../../components/SearchBar/SearchBar";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find real estate and get your dream place</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+            aperiam molestiae pariatur praesentium minus aut, amet saepe
+            sapiente, eligendi aliquid ducimus laboriosam soluta odit.
+            Asperiores accusantium impedit veritatis reiciendis dolore?
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>1200+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
