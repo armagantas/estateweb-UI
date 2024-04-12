f7005b581d5c73e4bbd59d970f80ea0976ce4370
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-12 22:36:42 +0300

    search bar and responsive features done.

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import "./SearchBar.scss";
+
+const types = ["buy", "rent"];
 
 const SearchBar = () => {
-  return <div className="searchBar">SearchBar</div>;
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (value) => {
+    setQuery((prev) => ({ ...prev, type: value }));
+  };
+  return (
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
+      <form>
+        <input type="text" name="location" placeholder="City location" />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={10000000}
+          placeholder="Min price"
+        />
+        <input
+          type="number"
+          name="maxPrice"
+          min={0}
+          max={10000000}
+          placeholder="Max price"
+        />
+        <button>
+          <img src="/search.png" alt="" />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
