a5c3de57388f16a2e18e3d5edd828e1d82c75d5a
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-15 02:19:34 +0300

    Profile page desing integrated.

--- src/routes/Profile/ProfilePage.jsx
+++ src/routes/Profile/ProfilePage.jsx
@@ -1,7 +1,42 @@
+import List from "../../components/List/List";
 import "./ProfilePage.scss";
 
 const ProfilePage = () => {
-  return <div className="profilePage">ProfilePage</div>;
+  return (
+    <div className="profilePage">
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h1>User Information</h1>
+            <button>Update Profile</button>
+          </div>
+          <div className="info">
+            <span>
+              Avatar: <img src="/person.jpeg" alt="" />
+            </span>
+            <span>
+              Username: <b>Maria Castellanos</b>
+            </span>
+            <span>
+              Email: <b>maria@gmail.com</b>
+            </span>
+          </div>
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Post</button>
+          </div>
+          <List />
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
+          <List />
+        </div>
+      </div>
+      <div className="chatContainer">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
