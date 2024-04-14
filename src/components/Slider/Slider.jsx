11610b3e9a95976ff938d44c898fc218bbc9dd2c
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-15 00:14:18 +0300

    Slider structure is done.

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,6 +1,40 @@
+import { useState } from "react";
 import "./Slider.scss";
-const Slider = () => {
-  return <div className="slider">Slider</div>;
+const Slider = ({ images }) => {
+  const [imageIndex, setImageIndex] = useState(null);
+  return (
+    <div className="slider">
+      {imageIndex !== null && (
+        <div className="fullSlider">
+          <div className="arrow">
+            <img src="/arrow.png" alt="" />
+          </div>
+          <div className="imgContainer">
+            <img src={images[imageIndex]} alt="" />
+          </div>
+          <div className="arrow">
+            <img src="/arrow.png" className="right" alt="" />
+          </div>
+          <div className="close" onClick={() => setImageIndex(null)}>
+            X
+          </div>
+        </div>
+      )}
+      <div className="bigImage">
+        <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
+      </div>
+      <div className="smallImages">
+        {images.slice(1).map((image, index) => (
+          <img
+            src={image}
+            alt=""
+            key={index}
+            onClick={() => setImageIndex(index + 1)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
